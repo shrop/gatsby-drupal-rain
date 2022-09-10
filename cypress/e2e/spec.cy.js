4b63806c3716847d15fc11b94684ebf78625c4d7
Mark Shropshire <mark.shropshire@mediacurrent.com>
2022-09-10 12:37:39 -0400

    Cypress setup
    * Updated gitignore to ignore Cypress asset downloads for tests
    * Updated some new Rain tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,3 +3,13 @@ describe('empty spec', () => {
     cy.visit('http://localhost:8000/')
   })
 })
+
+describe("Accessibility tests", () => {
+  beforeEach(() => {
+      cy.visit("http://localhost:8000/").get("main")
+      cy.injectAxe()
+  })
+  it("Has no detectable accessibility violations on load", () => {
+    cy.checkA11y()
+  })
+})
