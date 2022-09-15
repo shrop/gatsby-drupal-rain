e231989d931d0a428fb00556b5f070658aa18386
Mark Shropshire <mark.shropshire@mediacurrent.com>
2022-09-15 17:18:16 -0400

    Updated main spec file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,7 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8000/')
-  })
-})
-
-describe("Accessibility tests", () => {
-  beforeEach(() => {
-      cy.visit("http://localhost:8000/").get("main")
-      cy.injectAxe()
-  })
-  it("Has no detectable accessibility violations on load", () => {
-    cy.checkA11y()
+    cy.get('.chakra-heading').contains('Rain Demo')
+    //cy.screenshot()
   })
 })
